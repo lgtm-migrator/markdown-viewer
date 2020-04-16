7f5d1f3d7dc52626b89dbd0e0b6281a48cc74c54
Yvan Sraka <yvan@sraka.xyz>
2020-04-16 22:48:52 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+use wasm_bindgen::prelude::*;
+
+use pulldown_cmark::{Parser, Options, html};
+
+// Markdown -> HTML
+#[wasm_bindgen]
+pub fn render(markdown_input: &str) -> String {
+    // Set up options and parser. Strikethroughs are not part of the CommonMark standard
+    // and we therefore must enable it explicitly.
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    let parser = Parser::new_ext(markdown_input, options);
+    
+    // Write to String buffer.
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+    return html_output;
+}
+
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
+
+    let markdown_input = "
+# Title
+## subtitle
+**bold** and _emphasis_ text
+";
+
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_inner_html(
+        &render(markdown_input)
+    );
+
+    body.append_child(&val)?;
+
+    Ok(())
+}
